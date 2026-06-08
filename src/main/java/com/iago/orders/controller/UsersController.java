f5594b004718842b2bbae3e6bec8e933dd7bab65
pearljago <pearl.jago@gmail.com>
2026-06-08 12:49:12 +0200

    added structure

--- src/main/java/com/iago/orders/controller/UsersController.java
+++ src/main/java/com/iago/orders/controller/UsersController.java
@@ -1,4 +1,4 @@
-package com.iago.orders;
+package com.iago.orders.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
