6c334e674df38fdd4e14555b4aace1f9ee2eee98
pearljago <pearl.jago@gmail.com>
2026-06-08 15:20:50 +0200

    renamed app

---
+++ src/main/java/com/iago/orders/IagoOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.iago.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IagoOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IagoOrdersApplication.class, args);
+    }
+}
+
