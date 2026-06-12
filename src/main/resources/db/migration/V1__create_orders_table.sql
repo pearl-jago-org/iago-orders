a672d3d114f81c6ad4872f1f3ea12a718a45ccb9
pearljago <pearl.jago@gmail.com>
2026-06-12 14:15:10 +0200

    changed pom

--- src/main/resources/db/migration/V1__create_orders_table.sql
+++ src/main/resources/db/migration/V1__create_orders_table.sql
@@ -3,8 +3,5 @@ CREATE TABLE IF NOT EXISTS orders (
     user_id UUID NOT NULL,
     product VARCHAR(100) NOT NULL,
     amount NUMERIC(10,2) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    CONSTRAINT fk_user
-        FOREIGN KEY (user_id)
-        REFERENCES users(id)
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
