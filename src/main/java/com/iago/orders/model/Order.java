8ab8e429f784f041133ed21ffe3f62e9e690f9aa
pearljago <pearl.jago@gmail.com>
2026-06-17 12:02:37 +0200

    model correction

--- src/main/java/com/iago/orders/model/Order.java
+++ src/main/java/com/iago/orders/model/Order.java
@@ -1,14 +1,17 @@
 package com.iago.orders.model;
 
 import jakarta.persistence.*;
+import java.util.UUID;
 
 @Entity
 @Table(name = "orders")
 public class Order {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue
+    private UUID id;
+
+    private UUID userId;
 
     private String product;
 
@@ -16,13 +19,18 @@ public class Order {
 
     public Order() {}
 
-    public Order(String product, Double amount) {
+    public Order(UUID userId, String product, Double amount) {
+        this.userId = userId;
         this.product = product;
         this.amount = amount;
     }
 
-    public Long getId() {
+    public UUID getId() {
         return id;
+    }
+
+    public UUID getUserId() {
+        return userId;
     }
 
     public String getProduct() {
@@ -31,6 +39,10 @@ public class Order {
 
     public Double getAmount() {
         return amount;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
     }
 
     public void setProduct(String product) {
